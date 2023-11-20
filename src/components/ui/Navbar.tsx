54eee6a5e27031f38ad29dfaaef2c5fa79b7e85a
eQu33nox <equueenox@gmail.com>
2023-11-20 18:59:45 +0600

    modal

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { checkUserLogin, logout } from "../../helpers/functions";
+import UsersModal from "./UsersModal";
 
 const Navbar = () => {
+  const [usersModal, setUsersModal] = useState(false);
+
+  const toggleMenu = () => {
+    setUsersModal(!usersModal);
+  };
+
+  function closeUsersModal() {
+    setUsersModal(false);
+  }
+
   return (
     <div>
       <NavLink to="/">Home</NavLink>
@@ -22,6 +33,27 @@ const Navbar = () => {
         <>
           <NavLink to="/sign-up">Registration</NavLink>
           <NavLink to="/sign-in">Authorization</NavLink>
+          <NavLink to="/roadmaps">Road Maps</NavLink>
+          {/* Оставляйте модалку последней */}
+          <div className="users--modal">
+            <button className="modalBtn" onClick={toggleMenu}>
+              {usersModal ? (
+                <div className="close--modal">
+                  <p>Users name</p>
+                </div>
+              ) : (
+                <div className="open--modal">
+                  <p>Your Account</p>
+                </div>
+              )}
+            </button>
+            {usersModal && (
+              <div>
+                <UsersModal />
+                <div className="overlay" onClick={closeUsersModal}></div>
+              </div>
+            )}
+          </div>
         </>
       )}
     </div>
