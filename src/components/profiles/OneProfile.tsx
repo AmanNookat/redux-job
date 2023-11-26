96ddf93d9a83fbe45aaa9a9cf78eb9a868094b52
AmanNookat <138446723+AmanNookat@users.noreply.github.com>
2023-11-26 13:34:22 +0600

    резюме

--- src/components/profiles/OneProfile.tsx
+++ src/components/profiles/OneProfile.tsx
@@ -8,11 +8,13 @@ import {
   uploadResumeFile,
 } from "../../store/profiles/profilesActions";
 import ChangePassModal from "./ChangePassModal";
+import ResumeModal from "../resume/ResumeModal";
 
 const OneProfile = () => {
   const [modal, setModal] = useState(false);
   const [resumeFile, setResumeFile] = useState(null);
   const [edit, setEdit] = useState(false);
+  const [resumeModal, setResumeModal] = useState(false);
 
   const { oneProfile } = useSelector((state: RootState) => state.profiles);
 
@@ -27,132 +29,152 @@ const OneProfile = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      {edit ? (
-        <div>
-          <div>
-            <p>Языки:</p>
-            <input type="text" />
-          </div>
-          <div>
-            <p>Языки программирования:</p>
-            <input type="text" />
-          </div>
-          <div>
-            <p>Образование:</p>
-            <input type="text" />
-          </div>
-          <div>
-            <p>Стэк:</p>
-            <input type="text" />
-          </div>
-          <div>
-            <p>О себе:</p>
-            <input type="text" />
-          </div>
-          <div>
-            <p>О себе:</p>
-            <input type="text" />
-          </div>
-          <div>
-            <p>Возраст:</p>
-            <input type="text" />
-          </div>
-          <div>
-            <p>Достижения:</p>
-            <input type="text" />
-          </div>
-          <div>
-            <img src="" alt="" />
-          </div>
-        </div>
-      ) : (
-        <div>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <p>Языки: {oneProfile?.languages} </p>
-            <p>Языки программирования: {oneProfile?.programming_languages}</p>
-            <p>Образование:{oneProfile?.education}</p>
-            <p>Стэк:{oneProfile?.stack} </p>
-            <p>О себе:{oneProfile?.about}</p>
-            <p>Возраст: {oneProfile?.age}</p>
-            <p>Опыт работы:{oneProfile?.work_experience}</p>
-            <p>Достижения:{oneProfile?.work_experience}</p>
-            <img src={oneProfile?.profile_image} alt="kotak" />
-          </div>
-          <button style={{ color: "red" }}>Редактировать:</button>
-        </div>
+    <>
+      {resumeModal && (
+        <ResumeModal
+          resume={oneProfile?.resume1[0]}
+          setResumeModal={setResumeModal}
+        />
       )}
-      <a
-        onClick={() => setModal(true)}
-        className="text-blue-400 underline cursor-pointer"
-      >
-        Change password
-      </a>
-      {modal && <ChangePassModal setModal={setModal} />}
       <div>
-        {oneProfile?.upload_resume.length ? (
-          <>
-            <p>resume</p>
-
-            {oneProfile?.upload_resume[0].upload_file.includes(".jpg") ||
-            oneProfile?.upload_resume[0].upload_file.includes(".png") ||
-            oneProfile?.upload_resume[0].upload_file.includes(".jpeg") ||
-            oneProfile?.upload_resume[0].upload_file.includes(".svg") ? (
-              <img
-                src={`http://34.136.151.2${oneProfile?.upload_resume[0].upload_file}`}
-                alt="kchau"
-                width="100"
-              />
-            ) : (
-              <button className="bg-gray-500 p-2">
-                <a
-                  href={`http://34.136.151.2${oneProfile?.upload_resume[0].upload_file}`}
-                  target="_blanck"
-                >
-                  Open File
-                </a>
-              </button>
-            )}
-            <button
-              onClick={() => {
-                dispatch(
-                  deleteResumeFile({
-                    resumeId: oneProfile?.upload_resume[0].id,
-                    id: +id!,
-                  })
-                );
-              }}
-            >
-              Delete
-            </button>
-          </>
+        {edit ? (
+          <div>
+            <div>
+              <p>Языки:</p>
+              <input type="text" />
+            </div>
+            <div>
+              <p>Языки программирования:</p>
+              <input type="text" />
+            </div>
+            <div>
+              <p>Образование:</p>
+              <input type="text" />
+            </div>
+            <div>
+              <p>Стэк:</p>
+              <input type="text" />
+            </div>
+            <div>
+              <p>О себе:</p>
+              <input type="text" />
+            </div>
+            <div>
+              <p>О себе:</p>
+              <input type="text" />
+            </div>
+            <div>
+              <p>Возраст:</p>
+              <input type="text" />
+            </div>
+            <div>
+              <p>Достижения:</p>
+              <input type="text" />
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
         ) : (
-          <>
-            <label>Upload resume</label>
-            <input
-              type="file"
-              onChange={(e: any) => {
-                const selectedFile = e.target.files[0];
-                setResumeFile(selectedFile);
-              }}
-            />
-            {resumeFile && (
+          <div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <p>Языки: {oneProfile?.languages} </p>
+              <p>Языки программирования: {oneProfile?.programming_languages}</p>
+              <p>Образование:{oneProfile?.education}</p>
+              <p>Стэк:{oneProfile?.stack} </p>
+              <p>О себе:{oneProfile?.about}</p>
+              <p>Возраст: {oneProfile?.age}</p>
+              <p>Опыт работы:{oneProfile?.work_experience}</p>
+              <p>Достижения:{oneProfile?.work_experience}</p>
+              <img src={oneProfile?.profile_image} alt="kotak" />
+            </div>
+            <button style={{ color: "red" }}>Редактировать:</button>
+          </div>
+        )}
+        <a
+          onClick={() => setModal(true)}
+          className="text-blue-400 underline cursor-pointer"
+        >
+          Change password
+        </a>
+        {modal && <ChangePassModal setModal={setModal} />}
+        <div>
+          {oneProfile?.upload_resume.length ? (
+            <>
+              <p>resume</p>
+
+              {oneProfile?.upload_resume[0].upload_file.includes(".jpg") ||
+              oneProfile?.upload_resume[0].upload_file.includes(".png") ||
+              oneProfile?.upload_resume[0].upload_file.includes(".jpeg") ||
+              oneProfile?.upload_resume[0].upload_file.includes(".svg") ? (
+                <img
+                  src={`http://34.136.151.2${oneProfile?.upload_resume[0].upload_file}`}
+                  alt="kchau"
+                  width="100"
+                />
+              ) : (
+                <button className="bg-gray-500 p-2">
+                  <a
+                    href={`http://34.136.151.2${oneProfile?.upload_resume[0].upload_file}`}
+                    target="_blanck"
+                  >
+                    Open File
+                  </a>
+                </button>
+              )}
               <button
-                className="bg-green-500 p-2"
-                onClick={() =>
-                  dispatch(uploadResumeFile({ resumeFile, id: +id! }))
-                }
+                onClick={() => {
+                  dispatch(
+                    deleteResumeFile({
+                      resumeId: oneProfile?.upload_resume[0].id,
+                      id: +id!,
+                    })
+                  );
+                }}
               >
-                Save Resume
+                Delete
               </button>
-            )}
-          </>
+            </>
+          ) : (
+            <>
+              <label>Upload resume</label>
+              <input
+                type="file"
+                onChange={(e: any) => {
+                  const selectedFile = e.target.files[0];
+                  setResumeFile(selectedFile);
+                }}
+              />
+              {resumeFile && (
+                <button
+                  className="bg-green-500 p-2"
+                  onClick={() =>
+                    dispatch(uploadResumeFile({ resumeFile, id: +id! }))
+                  }
+                >
+                  Save Resume
+                </button>
+              )}
+            </>
+          )}
+        </div>
+        {oneProfile?.resume1.length ? (
+          <button
+            onClick={() => setResumeModal(true)}
+            className="bg-yellow-500"
+          >
+            See Resume
+          </button>
+        ) : (
+          <button
+            className="bg-blue-400 p-2"
+            onClick={() => navigate("/resume")}
+          >
+            Create Resume
+          </button>
         )}
       </div>
-      <button className="bg-blue-400 p-2" onClick={() => navigate("/resume")}>
-        Create Resume
-      </button>
-    </div>
+    </>
   );
 };
 
